35d542ae0b1c6d3ea1ad66455411dd9d7b8d156f
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2022-11-15 21:29:41 +0700

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
-
-using namespace std;
-
-int main(){
-    string number;
-    cin>>number;
-
-    for(int i = 0;i < number.size();i++){
-        for(int j = i + 1;j < number.size();j++){
-            if(number[i] == number[j]){
-                cout<<"true";
-                return 0;
-            }
-        }
-    }
-    cout<<"false";
-    return 0;
+void outputAnswer(int counter);
+int isEqualNumbers(int* number);
+int* inputNumber();
+int main() {
+	outputAnswer(isEqualNumbers(inputNumber()));
+	return 0;
+}
+int* inputNumber() {
+	int number;
+	std::cin >> number;
+	return &number;
+}
+int isEqualNumbers(int* number) {
+	int equalNumber;
+	int counter = 0;
+	int count = 0;
+	int arrayEqualNumbers[1024];
+	for (int i = 1; i < *number; i *= 10) {
+		if (i < 1000) equalNumber = (*number / i) % 10;
+		if (i > 999) equalNumber = *number / i;
+		arrayEqualNumbers[counter] = equalNumber;
+		counter++;
+	}
+	for (int i = 0; i < counter; i++) {
+		for (int j = 0; j < counter; j++) {
+			if (i == j) continue;
+			if (arrayEqualNumbers[i] == arrayEqualNumbers[j]) count++;
+		}
+	}
+	return count;
+}
+void outputAnswer(int counter) {
+	if (counter != 0) std::cout << "true";
+	if (counter == 0) std::cout << "false";
 }
